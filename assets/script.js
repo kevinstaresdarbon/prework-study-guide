432cf36bf7527d2701c14332d8699f4e8c5c9616
kevinstaresdarbon <red.inshou@gmail.com>
2023-09-10 13:47:55 +0100

    Added further logic to the js file to list topics and randomly select one displaying the results in the console. Also updated the index.html to show my notes for JavaScript section

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,35 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
-for(var x=0; x < topics.length; x++){
-    console.log(topics[x]);
-};
+function listTopics(){
+    for(var x=0; x < topics.length; x++){
+        console.log(topics[x]);
+    };
+}
 
-/*  
-commented out the entire block
+
 var topic = "HTML";
 
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}
-*/
+function selectTopic(x) {
+    if (x === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (x === 'CSS') {
+    console.log("Let's study CSS!");
+    } else if (x === 'Git') {
+    console.log("Let's study Git!");
+    } else if (x === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    } else {
+    console.log('Please try again!');
+    }
+};
+
+function rndOutput() {
+    var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+    selectTopic(randomTopic);
+};
+
+console.log('Here are the topics we learned in prework:');
+listTopics();
+
+console.log('Which topic shall we study today?');
+rndOutput();
